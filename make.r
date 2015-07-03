0c8b404ce61736ef24265e01b68d192b1d512037
François <briatte@gmail.com>
2015-07-03 10:38:51 +0200

    support for other disciplines

--- make.r
+++ make.r
@@ -33,12 +33,44 @@ dir.create("csv"   , showWarnings = FALSE)
 dir.create("html"  , showWarnings = FALSE)
 dir.create("plots" , showWarnings = FALSE)
 
-#
-# MAKE
-#
+# list of available disciplines
 
-data = "data/revues-soc.csv"
-html = "html/revues-soc-2015.html"
+base = "http://www.cairn.info/"
+uids = c(
+  "all"      = 0   ,
+  "arts"     = 70  ,
+  "droit"    = 2   ,
+  "eco"      = 1   ,
+  "educ"     = 8   ,
+  "general"  = 4   ,
+  "geo"      = 30  ,
+  "histoire" = 3   ,
+  "infocom"  = 9   ,
+  "lettres"  = 5   ,
+  "philo"    = 6   ,
+  "psycho"   = 7   ,
+  "sanpub"   = 141 ,
+  "soc"      = 11  ,
+  "scpo"     = 10  ,
+  "sport"    = 12
+)
+
+# set discipline here, or set to 'all' to download all disciplines
+
+# disc = "geo"
+stopifnot(disc %in% names(uids))
+
+data = paste0("data/revues-", disc, ".csv")
+html = paste0("html/revues-", disc, ifelse(disc == "all", "", "-"), "2015.html")
+arts = paste0("data/cairn-", disc, ".csv")
+
+# URL to sociology journals
+
+if(!file.exists(html))
+  download.file(ifelse(disc == "all", "https://www.cairn.info/listerev.php",
+                       paste0(base, "discipline.php?POS=",
+                              uids[ disc ], "&TITRE=ALL")),
+                html, mode = "wb", quiet = TRUE)
 
 source("01-data.r")
 source("02-clusters.r")
